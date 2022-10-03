4e1b8f2ae196c679d0cee2fcce47432324316b8f
Alexey <108893923+Alexey913@users.noreply.github.com>
2022-10-03 13:30:01 +0300

    Delete Seminar1 directory

--- Seminar1/Task1_1.py
+++
@@ -1,9 +0,0 @@
-#Проверяем, является ли одно число квадратом другого
-
-a = int(input())
-b = int(input())
-
-if a == b ** 2 or b == a ** 2:
-    print("yes")
-else:
-    print("no")
--- Seminar1/Task1_3.py
+++
@@ -1,3 +0,0 @@
-n = int (input('Введите N: '))
-for i in range (-n, n+1):
-    print (i, end =" ")
